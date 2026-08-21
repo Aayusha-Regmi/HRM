3e7e91bb44ee5eaf2d305b9d5d178be89f4f2954
Aayusha-Regmi <aayusharegmi2@gmail.com>
2026-08-21 19:38:44 +0000

    infra(iam): Attached IAM configuration for eks control plane and worker nodes including allowing network interface, ip addressess to be managed by aws vpc CNI running on worker node, ecr image read access policy also attached to iam worker role

---
+++ terraform/modules/eks/iam.tf
@@ -0,0 +1,67 @@
+#------------------------------------------
+#IAM configuration for the control plane
+#--------------------------------------------
+data "aws_iam_policy_document" "eks_cluster_assume_role"{
+    statement {
+       effect = "Allow"
+       actions= ["sts:AssumeRole"]
+
+       principals {
+        type = "Service"
+        identifiers= ["eks.amazonaws.com"]
+       }
+    }
+}
+
+# Create the IAM role for EKS cluster
+
+resource "aws_iam_role" "eks_cluster_role"{
+    name = "eks_cluster_role"
+    assume_role_policy= data.aws_iam_policy_document.eks_cluster_assume_role.json
+}
+
+# attach aws eks cluster policy to this eks_cluster_role
+resource "aws_iam_role_policy_attachment" "eks_cluster_role_policy"{
+    role= aws_iam_role.eks_cluster_role.name
+    policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+}
+
+#-------------------------------------------------------
+# IAM configuration for worker nodes 
+#=============================-----
+
+data "aws_iam_policy_document" "eks_worker_assume_role"{
+    statement {
+        effect = "Allow"
+        actions = ["sts:AssumeRole"]
+    
+        principals {
+            type = "Service"
+            identifiers = ["ec2.amazonaws.com"]
+        }
+}
+}
+
+# create the IAM role for worker nodes
+resource "aws_iam_role" "eks_worker_role"{
+    name= "eks_worker_role"
+    assume_role_policy= data.aws_iam_policy_document.eks_worker_assume_role.json
+}
+
+#attach aws eks worker policy to worker role
+resource "aws_iam_role_policy_attachment" "eks_worker_role_policy" {
+    role= aws_iam_role.eks_worker_role.name
+    policy_arn= "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+}
+
+#=======attach the policy to allow VPC 
+resource "aws_iam_role_policy_attachment" "eks_cni_policy"{
+    role= aws_iam_role.eks_worker_role.name
+    policy_arn= "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+}
+
+#=========attach policy to allow image pull from ecr
+resource "aws_iam_role_policy_attachment" "ecr_pull_policy"{
+    role= aws_iam_role.eks_worker_role.name
+    policy_arn= "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+}
